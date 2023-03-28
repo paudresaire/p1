32465609a414c547f0b7f5f451499c2bb8b5fd04
paudresaire <pau.dresaire@estudiantat.upc.edu>
2023-03-28 15:50:11 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <stdio.h>
+
+
+int pin_led = 1;
+
+void setup() {
+   pinMode(pin_led, OUTPUT);
+   //Serial.begin(115200);
+}
+
+void loop() {
+    digitalWrite(pin_led, HIGH);
+    delay(500);
+   // Serial.println("ON");
+    digitalWrite(pin_led, LOW);
+    delay(500);
+   // Serial.println("OFF");
+}
